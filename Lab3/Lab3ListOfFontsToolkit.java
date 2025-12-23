19ca3c2f9de60e118196a8cff74c70aa5a2eb417
esraaehab333 <esraaehabb333@gmail.com>
2025-12-23 09:20:01 +0200

    Fix panel class name in Lab3ListOfFontsToolkit to ensure proper functionality

--- Lab3/Lab3ListOfFontsToolkit.java
+++ Lab3/Lab3ListOfFontsToolkit.java
@@ -8,7 +8,7 @@ import java.awt.Toolkit;
 class Lab3ListOfFontsToolkit {
     public static void main(String[] args) {
         JFrame frame = new JFrame("List of Fonts");
-        MyPanel1 panel = new MyPanel1();
+        MyPanel panel = new MyPanel();
         frame.add(panel);
         frame.setSize(800, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,11 +19,9 @@ class Lab3ListOfFontsToolkit {
 class MyPanel1 extends JPanel {
     Toolkit tk = Toolkit.getDefaultToolkit();
     String[] fonts = tk.getFontList();  
-
     @Override
     protected void paintComponent(Graphics g) { 
         super.paintComponent(g);
-
         int y = 20;
         for (String fontName : fonts) {
             Font font = new Font(fontName, Font.BOLD, 18);
